59d4f60589a1e032df8bf5adc69875e6e7d65eb4
Renato S. de Araujo <136009548+renato1706@users.noreply.github.com>
2023-12-07 21:00:11 -0300

    informações salvas e carregadas no localstorage

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const notas = document.querySelectorAll(".nota");
 const medias = document.querySelectorAll(".media");
-const situacao = document.querySelectorAll(".situacao")
+const situacao = document.querySelectorAll(".situacao");
 
+document.addEventListener("DOMContentLoaded", () => {
+    for (let n = 0; n < notas.length; n++) {
+        const savedValue = localStorage.getItem(`nota_${n}`);
+        if (savedValue !== null) {
+            notas[n].value = savedValue;
+        }
+    }
+
+    calcularMedia();
+});
 
 for (let n = 0; n < notas.length; n++) {
-    notas[n].addEventListener("input", calcularMedia);
+    notas[n].addEventListener("input", () => {
+        localStorage.setItem(`nota_${n}`, notas[n].value);
+        calcularMedia();
+    });
 }
+
 function calcularMedia() {
     for (let n = 0; n < notas.length; n += 3) {
         const nota1 = parseFloat(notas[n].value) || 0;
@@ -17,14 +31,23 @@ function calcularMedia() {
 
         medias[n / 3].textContent = media.toFixed(2);
 
-        if (media >= 6){
-            situacao[n/3].textContent = "Aprovado";
-            situacao[n/3].style.color= "white";
-            situacao[n/3].style.backgroundColor = "green";            
+        if (media >= 6) {
+            situacao[n / 3].textContent = "Aprovado";
+            situacao[n / 3].style.color = "white";
+            situacao[n / 3].style.backgroundColor = "green";
         } else {
-            situacao[n/3].textContent = "Reprovado";
-            situacao[n/3].style.color = "white";
-            situacao[n/3].style.backgroundColor = "red";
+            situacao[n / 3].textContent = "Reprovado";
+            situacao[n / 3].style.color = "white";
+            situacao[n / 3].style.backgroundColor = "red";
         }
     }
 }
+
+const limparButton = document.createElement("button");
+limparButton.textContent = "Limpar";
+limparButton.addEventListener("click", () => {
+    localStorage.clear();
+    location.reload(); 
+});
+
+document.body.appendChild(limparButton);
